f139ad2755fc48268de4563f73fd5a07433d453d
Babalola-Yusuf <yusufbabalola01@gmail.com>
2026-01-25 12:00:46 +0100

    modified src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,18 @@
 // FILE: src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/autoplay';
+
+window.addEventListener('unhandledrejection', (e) => {
+  console.error('Unhandled promise rejection:', e.reason);
+});
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <HashRouter/*  basename="/react-vite-tailwind-ecommerce" */>
